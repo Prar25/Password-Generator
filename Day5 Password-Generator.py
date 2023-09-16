1e73d1a5508b66712156f47cba7e1773f6106809
Prarthana P <88665058+Prar25@users.noreply.github.com>
2023-09-16 11:48:00 +0530

    Add files via upload

---
+++ Day5 Password-Generator.py
@@ -0,0 +1,15 @@
+import random
+lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+upper_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Random Password Generator!")
+nr_loletters= int(input("How many lower case letters would you like in your password?\n")) 
+nr_upletters= int(input("How many upper case letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = random.choices(lower_letters,k=nr_loletters) +  random.choices(upper_letters,k=nr_upletters) + random.choices(numbers,k=nr_numbers) +  random.choices(symbols,k=nr_symbols) 
+random.shuffle(password)
+print("Your generated password is: "+"".join(password))
